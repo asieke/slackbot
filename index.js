98334d7db928962cd21456d7a669c1c2fffa9e38
Alex Sieke <asieke@gmail.com>
2022-12-13 16:16:06 -0500

    debugging

--- index.js
+++ index.js
@@ -13,12 +13,14 @@ const PORT = process.env.PORT || 3000;
 // Create an endpoint for the 'ai' route
 app.post('/ai', async (req, res) => {
   // Log the request body to the console
-  console.log('YAY! I got a request!', req.body.event.text);
+  console.log('YAY! I got a request!', req.body.event);
 
-  const aiQuery = removeBracketText(req.body.event.text);
-  const aiResponse = await getOpenAIResponse(aiQuery);
-  const message = '```' + stripNewLines(aiResponse.text) + '```';
-  await sendSlackMessage(req.body.event.channel, message);
+  //const aiQuery = removeBracketText(req.body.event.text);
+  //const aiResponse = await getOpenAIResponse(aiQuery);
+  //const message = '```' + stripNewLines(aiResponse.text) + '```';
+  //get the current date and time as a string
+  const dt = new Date().toLocaleString();
+  await sendSlackMessage(req.body.event.channel, dt);
 
   res.json({ aiResponse });
 });
