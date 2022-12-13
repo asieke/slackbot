900e1c86e44ce2d493f6f6124e3027d33bfb3f1a
Alex Sieke <asieke@gmail.com>
2022-12-12 20:17:57 -0500

    added body-parsing

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 // Create an express app
 const app = express();
+app.use(bodyParser.json());
 
 // Create an endpoint for the 'ai' route
 app.post('/ai', (req, res) => {
   // Log the request body to the console
-  console.log(req.body);
+  console.log('HELLO THERE HOW ARE YOU', req.body);
+});
+
+// create a get endpoint for the 'ai' route
+app.get('/ai', (req, res) => {
+  // return a json object with the request body
+  res.json({ hello: 'world' });
 });
 
 // Start the server on port 3000
