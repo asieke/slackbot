bb169aa05dc52c27b8e237dad47d670227150aea
Alex Sieke <asieke@gmail.com>
2022-12-12 20:59:05 -0500

    added slack posting

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const axios = require('axios');
+const KEY = 'xoxb-2544746774-4518443850993-AKOrDbLVEcnUEu0BVK7aE5Wu';
 
 // Create an express app
 const app = express();
@@ -11,6 +13,9 @@ const PORT = process.env.PORT || 3000;
 app.post('/ai', (req, res) => {
   // Log the request body to the console
   console.log('YAY! I got a request!', req.body);
+
+  // Send a slack message to the same channel in req.body
+  sendSlackMessage(req.body.channel, 'Hello from the server!');
 
   res.json({ hello: 'world' });
 });
@@ -25,3 +30,20 @@ app.get('/ai', (req, res) => {
 app.listen(PORT, () => {
   console.log('Server listening on port 3000');
 });
+
+const sendSlackMessage = (CHANNEL_ID, MESSAGE_TEXT) => {
+  const payload = {
+    token: KEY,
+    channel: CHANNEL_ID,
+    text: MESSAGE_TEXT,
+  };
+
+  axios
+    .post('https://slack.com/api/chat.postMessage', payload)
+    .then((response) => {
+      console.log(response.data);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+};
